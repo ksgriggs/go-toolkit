f836329ac8bf0efacece060fabf78399978f6601
Scott Griggs <sgriggs@gmail.com>
2024-07-21 18:12:50 -0400

    Add CreateDirIfNotExist method

--- tools_test.go
+++ tools_test.go
@@ -148,3 +148,22 @@ func TestTools_UploadOneFile(t *testing.T) {
 	// cleanup
 	_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName))
 }
+
+func TestTools_CreateDirIfNotExist(t *testing.T) {
+	var testTool Tools
+
+	err := testTool.CreateDirIfNotExist("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testTool.CreateDirIfNotExist("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = os.Remove("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+}
